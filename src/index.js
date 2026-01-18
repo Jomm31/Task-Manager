274933d1703db922fe2d50c56f24e26e796fb259
Jomm31 <jomm21212@gmail.com>
2026-01-18 16:19:33 +0800

    redux persist - local storage

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import store from './store/store';
+import store, { persistor } from './store/store';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={<div>Loading...</div>} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
--- src/store/store.js
+++ src/store/store.js
@@ -1,18 +1,40 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // localStorage
+import { combineReducers } from 'redux';
 import taskSlice from "../reducers/taskSlice"
 import projectReducer from "../reducers/projectReducer";
 import columnReducer from "../reducers/columnReducer";
 
+// Configure what to save to localStorage
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['tasks', 'projects', 'columns'] // Save these to localStorage
+};
 
-// can use redux devtools by using configureStore, Install Redux DevTools Extension in Chrome/Firefox
+// Combine all reducers
+const rootReducer = combineReducers({
+  tasks: taskSlice,
+  projects: projectReducer,
+  columns: columnReducer
+});
 
+// Wrap reducer with persistence
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// Create store with persisted reducer
 const store = configureStore({
-  reducer: {
-    tasks: taskSlice,
-    projects: projectReducer,
-    columns: columnReducer
-  }
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        // Ignore redux-persist actions
+        ignoredActions: ['persist/PERSIST', 'persist/REHYDRATE']
+      }
+    })
+});
 
-})
-
+// Create persistor
+export const persistor = persistStore(store);
 export default store;
